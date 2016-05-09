eed6b8cd6bf26c09d510dbd48b55f91e65668edf
Robert Baggett <rbaggett@gmail.com>
2016-05-08 18:11:21 -0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wowCollectingApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wowCollectingApp
+ */
+angular.module('wowCollectingApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wowCollectingApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wowCollectingApp
+ */
+angular.module('wowCollectingApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
