c954a32121922a375c9e526895d55e228619da8b
Robert Baggett <rbaggett@gmail.com>
2016-05-15 15:35:34 -0700

    'updates'

--- app/pages/pets/pets.controller.js
+++ app/pages/pets/pets.controller.js
@@ -6,54 +6,71 @@
     .module('wowCollectionsUi')
     .controller('PetsController', PetsController);
 
-  function PetsController(characterFactory, masterFactory, utilFactory) {
+  function PetsController($scope, characterFactory, masterFactory, utilFactory) {
 
     /* local data */
     var vm = this;
     var characterPets = characterFactory.character.pets.collected;
+    var filters = {};
     var masterPets = masterFactory.data.pets;
+    var pets = [];
 
     /* scope data */
     vm.breeds = masterFactory.data.breeds;
-    vm.pageSize = 24;
+    vm.pageSize = 20;
     vm.pets = [];
 
+
     /* scope functions */
+    vm.filterCollected = filterCollected;
+    vm.filterPets = filterPets;
+    vm.filterQuality = filterQuality;
     vm.getPetTile = getPetTile;
 
 
     (function activate() {
-      processPets();
+      mergePets();
+      fixProblemPets();
+      sortPets(['creatureName']);
+      pets = pets.concat(vm.pets);
       utilFactory.setActiveView('pets');
     })();
 
 
-    function processPets() {
-      addPetMetaData();
-      aggregatePets();
-      fixProblemPets();
-      console.log(vm.pets);
-    }
-
-
-    function addPetMetaData() {
-      var pet;
-      for (var count = 0, length = characterPets.length; count < length; count++) {
-        pet = characterPets[count];
-        pet.collected = true;
-        pet.theme = getPetTheme(pet);
-        pet.species = getPetSpecies(pet);
+    function filterCollected(collected) {
+      if (collected === undefined) {
+        filters = _.omit(filters, ['collected']);
       }
+      else {
+        filters.collected = collected;
+      }
+      filterPets();
     }
 
 
-    function aggregatePets() {
-      vm.pets = vm.pets
-        .concat(characterPets)
-        .concat(_.xorBy(characterPets, masterPets, 'creatureId'));
+    function filterPets() {
+      debugger
+      vm.pets.length = 0;
+      vm.pets = vm.pets.concat(pets);
+      vm.pets = _.filter(vm.pets, filters);
     }
 
 
+    function filterQuality(quality) {
+      if (quality === undefined) {
+        filters = _.omit(filters, ['qualityId']);
+      }
+      else {
+        filters.qualityId = quality;
+      }
+      filterPets();
+    }
+
+
+    /**
+     * Fix any outlier (problem) pets
+     * - i.e., bad icon string
+     */
     function fixProblemPets() {
       var petIndex = -1;
 
@@ -77,51 +94,86 @@
     }
 
 
-    function getPetSpecies(pet) {
-      var species = _.filter(masterPets, {creatureId: pet.creatureId});
-      if (species.length) {
-        return species[0];
-      }
-      return {};
-    }
-
-
+    /**
+     * Get the pet tile class
+     * @param pet {Object} - pet
+     * @returns {string} - class string
+     */
     function getPetTile(pet) {
-      var tileClass;
-      if (pet.collected) {
-        switch (pet.qualityId) {
-          case 3:
-            tileClass = 'pet-tile-primary';
-            break;
-          case 2:
-            tileClass = 'pet-tile-success';
-            break;
-          default:
-            tileClass = 'pet-tile-default';
-            break;
-        }
-      }
-      else {
-        tileClass = 'pet-tile-default-dashed';
-      }
-      return tileClass;
+      var classes = 'pet-tile-' + pet.theme;
+       if (pet.collected) {
+         classes += ' pet-tile-border-solid';
+      }else {
+         classes += ' pet-tile-border-dashed';
+       }
+      return classes;
     }
 
 
+    /**
+     * Get the pet theme
+     * - primary, success, default
+     * @param pet {Object} - pet
+     * @returns {string} - theme
+     */
     function getPetTheme(pet) {
       var theme;
-      switch (pet.qualityId) {
-        case 3:
-          theme = 'primary';
-          break;
-        case 2:
-          theme = 'success';
-          break;
-        default:
-          theme = 'default';
-          break;
+      // if (pet.collected) {
+        switch (pet.qualityId) {
+          case 4:
+          case 3:
+            theme = 'primary';
+            break;
+          case 2:
+            theme = 'success';
+            break;
+          default:
+            theme = 'default';
+            break;
+        }
+        return theme;
+      // }
+      // return 'default';
+    }
+
+
+    /**
+     * Merge the character and master pet lists
+     * - both arrays are merged, and data is supplmented
+     */
+    function mergePets() {
+      var cPets, mPet, nPet;
+
+      /* loop through the master pet list */
+      for (var i = 0, j = masterPets.length; i < j; i++) {
+        mPet = masterPets[i];
+        /* check character pets for matches */
+        cPets = _.filter(characterPets, {creatureId: mPet.creatureId});
+        if (cPets.length) {
+          /* loop through each match */
+          for (var k = 0, l = cPets.length; k < l; k++) {
+            nPet = _.merge(mPet, cPets[k]);
+            nPet.collected = true;
+            nPet.theme = getPetTheme(nPet);
+            vm.pets.push(nPet);
+          }
+          continue;
+        }
+        /* not collected */
+        mPet.collected = false;
+        mPet.creatureName = mPet.name;
+        mPet.theme = getPetTheme(mPet);
+        vm.pets.push(mPet);
       }
-      return theme;
+    }
+
+
+    /**
+     * Sort the pets based on properties
+     * @param properties {[string]} - properties for sorting
+     */
+    function sortPets(properties) {
+      vm.pets = _.sortBy(vm.pets, properties);
     }
 
   }
