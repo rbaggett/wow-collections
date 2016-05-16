75ee40d734c37c05f1e338ff06bd01f5425e2da3
Robert Baggett <rbaggett@gmail.com>
2016-05-15 20:00:12 -0700

    'updates'

--- app/pages/pets/pets.controller.js
+++ app/pages/pets/pets.controller.js
@@ -11,18 +11,34 @@
     /* local data */
     var vm = this;
     var characterPets = characterFactory.character.pets.collected;
-    var filters = {};
+    // var filters = {};
     var masterPets = masterFactory.data.pets;
     var pets = [];
 
     /* scope data */
     vm.breeds = masterFactory.data.breeds;
-    vm.pageSize = 20;
+    vm.filters = {};
+    vm.pageSize = 24;
     vm.pets = [];
+
+    // /* scope view state */
+    // vm.viewState = {
+    //   filters: {
+    //     collected: {
+    //       dupe: false,
+    //       have: false,
+    //       need: false
+    //     }
+    //   }
+    // };
 
 
     /* scope functions */
+    // vm.filterCollectionExtra = filterCollectionExtra;
     vm.filterCollected = filterCollected;
+    vm.filterDuplicate = filterDuplicate;
+    vm.filterNotCollected = filterNotCollected;
+    // vm.filterCollectionNeed = filterCollectionNeed;
     vm.filterPets = filterPets;
     vm.filterQuality = filterQuality;
     vm.getPetTile = getPetTile;
@@ -37,33 +53,54 @@
     })();
 
 
-    function filterCollected(collected) {
-      if (collected === undefined) {
-        filters = _.omit(filters, ['collected']);
+    function filterCollected() {
+      vm.filters = _.omit(vm.filters, ['notCollected', 'duplicate']);
+      if (vm.filters.original) {
+        vm.filters = _.omit(vm.filters, ['original']);
+      } else {
+        vm.filters.original = true;
       }
-      else {
-        filters.collected = collected;
+      filterPets();
+    }
+
+
+    function filterDuplicate() {
+      vm.filters = _.omit(vm.filters, ['original', 'notCollected']);
+      if (vm.filters.duplicate) {
+        vm.filters = _.omit(vm.filters, ['duplicate']);
+      } else {
+        vm.filters.duplicate = true;
+      }
+      filterPets();
+    }
+
+
+    function filterNotCollected() {
+      vm.filters = _.omit(vm.filters, ['original', 'duplicate']);
+      if (vm.filters.notCollected) {
+        vm.filters = _.omit(vm.filters, ['notCollected']);
+      } else {
+        vm.filters.notCollected = true;
       }
       filterPets();
     }
 
 
     function filterPets() {
-      debugger
       vm.pets.length = 0;
       vm.pets = vm.pets.concat(pets);
-      vm.pets = _.filter(vm.pets, filters);
+      vm.pets = _.filter(vm.pets, vm.filters);
     }
 
 
     function filterQuality(quality) {
-      if (quality === undefined) {
-        filters = _.omit(filters, ['qualityId']);
-      }
-      else {
-        filters.qualityId = quality;
-      }
-      filterPets();
+      // if (quality === undefined) {
+      //   filters = _.omit(filters, ['qualityId']);
+      // }
+      // else {
+      //   filters.qualityId = quality;
+      // }
+      // filterPets();
     }
 
 
@@ -101,11 +138,11 @@
      */
     function getPetTile(pet) {
       var classes = 'pet-tile-' + pet.theme;
-       if (pet.collected) {
-         classes += ' pet-tile-border-solid';
-      }else {
-         classes += ' pet-tile-border-dashed';
-       }
+      if (pet.collected) {
+        classes += ' pet-tile-border-solid';
+      } else {
+        classes += ' pet-tile-border-dashed';
+      }
       return classes;
     }
 
@@ -119,19 +156,19 @@
     function getPetTheme(pet) {
       var theme;
       // if (pet.collected) {
-        switch (pet.qualityId) {
-          case 4:
-          case 3:
-            theme = 'primary';
-            break;
-          case 2:
-            theme = 'success';
-            break;
-          default:
-            theme = 'default';
-            break;
-        }
-        return theme;
+      switch (pet.qualityId) {
+        case 4:
+        case 3:
+          theme = 'primary';
+          break;
+        case 2:
+          theme = 'success';
+          break;
+        default:
+          theme = 'default';
+          break;
+      }
+      return theme;
       // }
       // return 'default';
     }
@@ -143,28 +180,41 @@
      */
     function mergePets() {
       var cPets, mPet, nPet;
+      // var foo = 0;
 
       /* loop through the master pet list */
       for (var i = 0, j = masterPets.length; i < j; i++) {
-        mPet = masterPets[i];
+        mPet = angular.copy(masterPets[i]);
         /* check character pets for matches */
         cPets = _.filter(characterPets, {creatureId: mPet.creatureId});
         if (cPets.length) {
           /* loop through each match */
           for (var k = 0, l = cPets.length; k < l; k++) {
-            nPet = _.merge(mPet, cPets[k]);
+            nPet = angular.copy(_.extend(mPet, cPets[k]));
             nPet.collected = true;
+
+            nPet.duplicate = (k > 0);
+            // console.log(nPet.duplicate)
+            // console.log(cPets[k].creatureName)
+            // console.log('------------------------')
+            nPet.notCollected = false;
+            nPet.original = (k === 0);
             nPet.theme = getPetTheme(nPet);
             vm.pets.push(nPet);
+            // if (k > 0) {
+            //   foo++;
+            // }
           }
           continue;
         }
         /* not collected */
-        mPet.collected = false;
+        nPet.collected = false;
         mPet.creatureName = mPet.name;
+        mPet.notCollected = true;
         mPet.theme = getPetTheme(mPet);
         vm.pets.push(mPet);
       }
+      // console.log('dupes:' + foo);
     }
 
 
