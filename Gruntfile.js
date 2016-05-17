997667ee1e174b5a365207c4d6cfdc4316699608
Robert Baggett <rbaggett@gmail.com>
2016-05-16 22:55:05 -0700

    'updates'

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,7 @@ module.exports = function (grunt) {
         tasks: ['newer:jshint:test', 'newer:jscs:test', 'karma']
       },
       compass: {
-        files: ['<%= yeoman.app %>/**/*.{scss,sass}'],
+        files: ['<%= yeoman.app %>/styles/**/*.{scss,sass}'],
         tasks: ['compass:server', 'postcss:server']
       },
       gruntfile: {
@@ -377,7 +377,7 @@ module.exports = function (grunt) {
           usemin: 'scripts/scripts.js'
         },
         cwd: '<%= yeoman.app %>',
-        src: 'pages/**/*.html',
+        src: '/pages/**/*.html',
         dest: '.tmp/templateCache.js'
       }
     },
